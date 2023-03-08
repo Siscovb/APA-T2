6903b3a3d67e9969dba57bdae3c330bad2246439
JoanMarc02 <joan.marc.fuentes@estudiantat.upc.edu>
2023-03-08 10:23:08 +0100

    Creamos la función "def primos(numero)" + test unitario

--- primos.py
+++ primos.py
@@ -10,9 +10,17 @@ def esPrimo(numero):
     Devuelve True si su argumento es primo, y False si no lo es.
     >>> [numero for numero in range(2,50) if esPrimo(numero)]
     [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
-
     """
+    
     for prueba in range(2,numero):
         if numero % prueba == 0:
             return False
     return True
+
+def primos(numero):
+    """
+    Devuelve una tupla con todos los números primos menores que su argumento.
+    >>> primos(50)
+    (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47)
+    """
+    return tuple([prueba for prueba in range(2,numero) if esPrimo(prueba)])
