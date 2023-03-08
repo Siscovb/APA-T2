87b92ec45f1c16e826a2f68a176ebeef1ae0cf2a
JoanMarc02 <joan.marc.fuentes@estudiantat.upc.edu>
2023-03-08 10:23:38 +0100

    Update primos.py

--- primos.py
+++ primos.py
@@ -11,7 +11,6 @@ def esPrimo(numero):
     >>> [numero for numero in range(2,50) if esPrimo(numero)]
     [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
     """
-    
     for prueba in range(2,numero):
         if numero % prueba == 0:
             return False
