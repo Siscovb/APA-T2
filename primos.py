7ce518a10825c3db3bedef1b233fe43b62beeda7
Adrià Serrán Grauvilardell <adria.serran@estudiantat.upc.edu>
2023-03-09 18:49:43 +0100

    Fins mcm

--- primos.py
+++ primos.py
@@ -10,14 +10,20 @@ Exemples:
 
 >>> primos(50)
 (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47)
+
+>>> descompon(36 * 175 * 143)
+(2, 2, 3, 3, 5, 5, 7, 11, 13)
+
+>>> mcm(90, 14)
+630
 """
 
 def esPrimo(numero):
     """
-    Devuelve true si el argumento es primo, y false si no lo es.
+    Devuelve true si el argumento es primo y false si no lo es.
     """
 
-    for prova in range(2, numero):
+    for prova in range(2, int (numero**0.5 + 1)):
         if numero % prova == 0:
             return False
 
@@ -28,6 +34,50 @@ def primos(numero):
     Devuelve...
     """
     return tuple([prova for prova in range(2, numero) if esPrimo(prova)])
+
+def descompon(numero):
+    """
+    Devuelve una **tupla** con la descomposición en factores primos de su argumento.
+    """
+
+    factores = tuple()
+    for factor in primos(numero + 1):
+        while numero % factor == 0:
+            numero = numero // factor
+            factores = factores + (factor,)
+    
+    return factores
+
+
+def fact2dic(numero1, numero2):
+    factores1 = descompon(numero1)
+    factores2 = descompon(numero2)
+    factores = set(factores1) | set(factores2)
+    dic1 = {factor : 0 for factor in factores}
+    dic2 = {factor : 0 for factor in factores}
+    for factor in factores1:
+        dic1[factor] += 1
+
+    for factor in factores2:
+        dic2[factor] += 1
+
+    return dic1, dic2
+
+
+def mcm(numero1, numero2):
+    """
+    Devuelve el mínimo común múltiplo de sus argumentos.
+    """
+    dic1, dic2 = fact2dic(numero1, numero2)
+    mcm = 1
+    for factor in dic1:
+        mcm *= factor**max(dic1[factor], dic2[factor])
+    return mcm
+
+
+
+
+
 
 
 import doctest
