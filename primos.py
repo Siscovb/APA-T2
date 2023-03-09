ab74231714b146c258c2c532aea0263f5d9e54d0
Kirian Rodríguez Alonso <kirian.rodriguez@estudiantat.upc.edu>
2023-03-09 18:49:09 +0100

    mcm hecho

--- primos.py
+++ primos.py
@@ -4,9 +4,21 @@ Kirian Rodríguez Alonso
 Módulo de gestión de números primos
 
 Exemples: 
->>> [ numero for numero in range(2, 50) if esPrimo(numero) ]
+ [ numero for numero in range(2, 50) if esPrimo(numero) ]
 La salida debe ser [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47].
+
+ primos(50)
+La salida debe ser (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47) 
+
+ descompon(36 * 175 * 143)
+La salida debe ser (2, 2, 3, 3, 5, 5, 7, 11, 13).
+
+
+Al ejecutar mcm(90, 14), la salida debe ser 630.
+Al ejecutar mcd(924, 780), la salida debe ser 12.
 """
+
+
 
 def esPrimo(numero):
     """
@@ -29,7 +41,7 @@ def primos(numero):
 
 def descompon(numero):
     """
-    Devuelve una tupla con la descomposicion en factos primos de su argumento
+    Devuelve una tupla con la descomposicion en factores primos de su argumento
     """
 
     factores=tuple()
@@ -38,6 +50,48 @@ def descompon(numero):
             numero=numero//factor
             factores=factores+(factor,)
     return factores
+
+
+
+def fact2dic(numero1, numero2):
+    
+    factores1 = descompon(numero1)
+    factores2 = descompon(numero2)
+    factores = set(factores1) | set(factores2)
+    "comprensiones -> {exprKey : exprVal for elemento in iterable if condicion}"
+    dic1 = {factor: 0 for factor in factores}
+    dic2 = {factor: 0 for factor in factores}
+    
+    for factor in factores1: 
+        dic1[factor] += 1
+
+    for factor in factores2: 
+        dic2[factor] += 1
+    
+    return dic1, dic2
+
+def mcm(numero1, numero2):
+    """
+    Devuelve el mínimo común múltiplo de sus argumentos.
+    """
+
+    dic1, dic2 = fact2dic(numero1,numero2)
+    mcm = 1
+    for factor in dic1:
+        mcm *= factor**max(dic1[factor], dic2[factor])
+    return mcm
+
+
+
+
+
+
+def mcd(numero1, numero2):
+    """
+    Devuelve el máximo común divisor de sus argumentos.
+    """
+
+
 
 import doctest
 doctest.testmod()
