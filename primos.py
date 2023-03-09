4e400da5fd84be040423e5728b75664bd09e4aaf
Milene Granda <milene.soledad.granda@estudiantat.upc.edu>
2023-03-09 17:08:41 +0100

    2

---
+++ primos.py
@@ -0,0 +1,32 @@
+"""
+Milene Granda
+
+Módulo de gestión de numeros primos
+
+Exemples:
+
+>>> [ numero for numero in range(2, 50) if esPrimo(numero) ]
+[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+
+>>> primos(50)
+(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47).
+"""
+
+def esPrimo(numero):
+    """
+    Devuelve True si su argumento es primo, y False si no lo es.
+    """
+
+    for proba in range(2, numero):
+        if numero % proba ==0:
+            return False
+    return True 
+
+def primos(numero):
+    """
+    Devuelve una tupla con todos los números primos menores que su argumento
+    """
+    return tuple([proba for proba in range(2,numero) if esPrimo(proba)])
+'esto es para hacer la prueba:'
+import doctest
+doctest.testmod()
