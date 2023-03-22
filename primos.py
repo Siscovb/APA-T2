cba38bdd7aaae71874f54a730449400ab37f66db
Adrià Serrán <adriaserran@gmail.com>
2023-03-22 13:30:03 +0100

    Entrega final

--- primos.py
+++ primos.py
@@ -2,28 +2,13 @@
 Adrià Serrán Grauvilardell
 
 Módulo de gestión de números primos
-
-Exemples:
-
->>> [ numero for numero in range(2, 50) if esPrimo(numero) ]
-[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
-
->>> primos(50)
-(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47)
-
->>> descompon(36 * 175 * 143)
-(2, 2, 3, 3, 5, 5, 7, 11, 13)
-
->>> mcm(90, 14)
-630
-
->>> mcmN(42, 60, 70, 63)
-1260
 """
 
 def esPrimo(numero):
     """
     Devuelve true si el argumento es primo y false si no lo es.
+    >>> [numero for numero in range(2, 50) if esPrimo(numero)]
+    [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
     """
 
     for prova in range(2, int (numero**0.5 + 1)):
@@ -35,12 +20,16 @@ def esPrimo(numero):
 def primos(numero):
     """
     Devuelve...
+    >>> primos(50)
+    (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47)
     """
     return tuple([prova for prova in range(2, numero) if esPrimo(prova)])
 
 def descompon(numero):
     """
     Devuelve una **tupla** con la descomposición en factores primos de su argumento.
+    >>> descompon(36 * 175 * 143)
+    (2, 2, 3, 3, 5, 5, 7, 11, 13)
     """
 
     factores = tuple()
@@ -70,6 +59,8 @@ def fact2dic(numero1, numero2):
 def mcm(numero1, numero2):
     """
     Devuelve el mínimo común múltiplo de sus argumentos.
+    >>> mcm(90, 14)
+    630
     """
     dic1, dic2 = fact2dic(numero1, numero2)
     mcm = 1
@@ -78,22 +69,42 @@ def mcm(numero1, numero2):
     return mcm
 
 
+def mcd(numero1, numero2):
+    """
+    Devuelve el máximo común divisor de sus argumentos.
+    >>> mcd(924, 780)
+    12
+    """
+    dic1, dic2 = fact2dic(numero1, numero2)
+    mcd = 1
+    for factor in dic1:
+        mcd *= factor**min(dic1[factor], dic2[factor])
+    return mcd
+    
+
 def mcmN(*numeros):
     """
-    Devuelve el mínimo común múltiplo de una lista de N números.
+    Devuelve el mínimo común múltiplo de sus argumentos.
+    >>> mcmN(42, 60, 70, 63)
+    1260
     """
-    comuns = {}
-    for numero in numeros:
-        factors = fact2dic(numero)
-        for factor, exponent in factors.items():
-            comuns[factor] = max(exponent, comuns.get(factor, 0))
-    mcm = 1
-    for factor, exponent in comuns.items():
-        mcm *= factor ** exponent
-    return mcm
+    l = len(numeros)
+    a = numeros[0]
+    for iter in range(0, l):
+        a = mcm(a, numeros[iter])
+    return a
 
-
-
+def mcdN(*numeros):
+    """
+    Devuelve el máximo común divisor de sus argumentos.
+    >>> mcdN(840, 630, 1050, 1470)
+    210
+    """
+    l = len(numeros)
+    a = numeros[0]
+    for iter in range(0, l):
+        a = mcd(a, numeros[iter])
+    return a
 
 
 import doctest
